8a65514e79345fdd32432e34c0847c029df0eb1c
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-04 19:43:45 -0400

    Created constructor

--- src/Grid.java
+++ src/Grid.java
@@ -1,9 +1,23 @@
 public class Grid {
 
-    private Square grid;
+    private Square[][] grid;
     private int width;
     private int height;
     private int numMines;
-    private int numSquaresUncovered;
+    private int numSquaresUncovered = 0;
+
+    public Grid(int width, int height, int numMines){
+        this.width = width;
+        this.height = height;
+        this.numMines = numMines;
+    }
+
+    public int getNeighbors(int row, int column){
+        return 0;
+    }
+
+    public Status uncoverSquare(int row, int column){
+        return Status.OK;
+    }
 
 }
