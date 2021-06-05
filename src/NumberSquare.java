3ba546d04287844a9122ad7a818d239c217d77e0
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-04 23:24:15 -0400

    I'm tired

--- src/NumberSquare.java
+++ src/NumberSquare.java
@@ -2,11 +2,21 @@ public class NumberSquare extends Square {
 
     private int neighborMines;
     private int myRow;
-    private int myColumn;
+    private int myCol;
+
+    public NumberSquare(int neighborMines, int myRow, int myCol){
+        this.neighborMines = neighborMines;
+        this.myRow = myRow;
+        this.myCol = myCol;
+    }
 
     @Override
     public boolean uncover() {
         return false;
+    }
+
+    public int getNeighborMines(){
+        return neighborMines;
     }
 
     @Override
