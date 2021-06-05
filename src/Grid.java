005a4070e7bb842520985a691f35f04c5b6e7da4
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-04 23:22:49 -0400

    Got initial grid constructor working

--- src/Grid.java
+++ src/Grid.java
@@ -1,15 +1,44 @@
+import java.util.Random;
+
 public class Grid {
 
     private Square[][] grid;
-    private int width;
-    private int height;
-    private int numMines;
     private int numSquaresUncovered = 0;
 
     public Grid(int width, int height, int numMines){
-        this.width = width;
-        this.height = height;
-        this.numMines = numMines;
+
+        // Create grid with given height and width
+        grid = new Square[height][width];
+
+        // Randomly populate grid with mines
+        // Create Random object
+        Random rand = new Random();
+
+        // Places requested number of Mines
+        // Increment argument not passed to prevent automatic incrementation if mine is not present
+        for(int i = 0; i < numMines; ){
+            // Uses Random object to generate random grid position
+            int randRow = rand.nextInt(height);
+            int randCol = rand.nextInt(width);
+
+            // Checks to make sure there isn't already a mine at the selected spot
+            if(!(grid[randRow][randCol] instanceof MineSquare)){
+                // If not already a mine puts down mine and increments for loop
+                grid[randRow][randCol] = new MineSquare();
+                i++;
+            }
+        }
+
+        for(int row = 0; row < height; row++){
+            for (int col = 0; col < width; col++)
+            {
+                if(!(grid[row][col] instanceof MineSquare)){
+                    grid[row][col] = new NumberSquare(5, row, col);
+                }
+                System.out.print(grid[row][col]);
+            }
+            System.out.println();
+        }
     }
 
     public int getNeighbors(int row, int column){
