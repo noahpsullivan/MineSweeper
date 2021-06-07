0103ca356af0ffb235448e2978b9469449c2fc2a
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-06 21:05:36 -0400

    Idek dude

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Minesweeper {
+    public void runGame(){
+        Grid gameBoard = new Grid(12, 10, 10);
+        Status gameState = Status.OK;
+        while(gameState == Status.OK){
+            gameState = Status.MINE;
+        }
+    }
+
+    public char getInput(){
+        Scanner keyboard = new Scanner(System.in);
+
+    }
 }
