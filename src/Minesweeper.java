b4ece795a6f62e5964c296bc8bedc0e872bfa45a
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-08 12:39:47 -0400

    Bailing out to my laptop because my desktop is borked

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -9,13 +9,48 @@ public class Minesweeper {
         }
     }
 
-    public void getInput(){
+    public ReturnInput getInput(){
         Scanner keyboard = new Scanner(System.in);
         String input = keyboard.nextLine();
-        input = input.replaceAll("\\s", "").toLowerCase();
-        int length = input.length();
-        if(length == 1 && input.equals("q")){
-            System.out.println("yuh");
+        String[] pieces = input.replaceAll("\\W", "").split("");
+        if(pieces.length != 1 && pieces.length != 3){
+            return new ReturnInput();
+        }
+        char action = pieces[0].charAt(0);
+        if(!Character.isAlphabetic(action)){
+            return new ReturnInput();
+        }
+    }
+
+    private class ReturnInput{
+        private char action;
+        private int row;
+        private int col;
+
+        public ReturnInput(char action, int row, int col){
+            this.action = action;
+            this.row = row;
+            this.col = col;
+        }
+
+        public ReturnInput(char action){
+            this(action, -1, -1);
+        }
+
+        public ReturnInput(){
+            this(Character.MIN_VALUE, -1, -1);
+        }
+
+        public char getAction() {
+            return action;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
         }
     }
 }
