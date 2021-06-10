b95fd8a7aaca187370be64589754fade1d56b1c9
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-10 14:36:49 -0400

    Removed Driver.java from MineSweeper package

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,13 @@
-package MineSweeper;
+import MineSweeper.Minesweeper;
 
+/**
+ * Driver class to run the Minesweeper grade.
+ */
 public class Driver {
 
+    /**
+     * Creates Minesweeper object and runs game.
+     */
     public static void main(String[] args) {
         Minesweeper gameInstance = new Minesweeper();
         gameInstance.runGame();
