a80c6d1cd075a59d3b7473776aa8655c505ffbf8
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-10 14:36:23 -0400

    Added Javadocs

--- src/MineSweeper/MineSquare.java
+++ src/MineSweeper/MineSquare.java
@@ -1,5 +1,9 @@
 package MineSweeper;
 
+/**
+ * <code>MineSquare</code> is the class that represents mined squares by extending {@link Square}. It provides methods
+ * to uncover the <code>Square</code> and determine if the <code>Square</code> is a mine.
+ */
 public class MineSquare extends Square {
 
     @Override
--- src/MineSweeper/Status.java
+++ src/MineSweeper/Status.java
@@ -1,5 +1,8 @@
 package MineSweeper;
 
+/**
+ * Possible game statuses after uncovering a square
+ */
 public enum Status {
     OK,
     WIN,
