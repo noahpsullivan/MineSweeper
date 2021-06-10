e03757247a681d1c2d273dfd68c865c6ef9429e3
noahpsullivan <57803357+noahpsullivan@users.noreply.github.com>
2021-06-10 14:22:14 -0400

    Added Javadocs

--- src/MineSweeper/NumberSquare.java
+++ src/MineSweeper/NumberSquare.java
@@ -1,11 +1,27 @@
 package MineSweeper;
 
+/**
+ * <code>NumberSquare</code> is the class that represents non-mined squares by extending {@link Square}. It provides
+ * methods to uncover the <code>Square</code>, determine the number of neighboring mines, and determining if the
+ * <code>Square</code> is a mine.
+ *
+ * @author Noah Sullivan
+ */
 public class NumberSquare extends Square {
 
-    private int neighborMines;
-    private int myRow;
-    private int myCol;
+    private final int neighborMines;
 
+    // These don't do anything
+    private final int myRow;
+    private final int myCol;
+
+    /**
+     * Constructor for <code>NumberSquare</code> taking neighboring mines, row number, and row column
+     *
+     * @param neighborMines int containing number of neighboring mines
+     * @param myRow         int containing row number
+     * @param myCol         int containing column number
+     */
     public NumberSquare(int neighborMines, int myRow, int myCol){
         this.neighborMines = neighborMines;
         this.myRow = myRow;
@@ -13,8 +29,12 @@ public class NumberSquare extends Square {
     }
 
     /**
+     * Attempts to uncover square, if able to uncover sets display <code>element</code>> accordingly. If there are no
+     * neighbor mines then the display <code>element</code> is set to "<code>_</code>"; otherwise it is set to the
+     * number of neighbor mines. Number of neighbor mines is retrieved using the
+     * {@link #getNeighborMines() getNeightborMines} method.
      *
-     * @return
+     * @return  boolean, <code>true</code> if uncover is successful, <code>false</code> if not
      */
     @Override
     public boolean uncover() {
@@ -33,18 +53,10 @@ public class NumberSquare extends Square {
         }
     }
 
-    /**
-     *
-     * @return
-     */
     public int getNeighborMines(){
         return neighborMines;
     }
 
-    /**
-     *
-     * @return
-     */
     @Override
     public boolean isMine() {
         return false;
